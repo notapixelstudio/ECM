542a00e8878d4cda7d56081820614dada2a2df6c
nitaku <matteo.abrate@gmail.com>
2019-03-01 23:33:58 +0100

    Autoload

--- main.gd
+++ main.gd
@@ -6,10 +6,14 @@ func _enter_tree():
 	add_custom_type("Entity", "Node", preload("Entity.gd"), preload("icons/Entity.png"))
 	add_custom_type("Component", "Node", preload("Component.gd"), preload("icons/Component.png"))
 	add_custom_type("Manager", "Node", preload("Manager.gd"), preload("icons/Manager.png"))
+	
+	add_autoload_singleton("ECM", "res://addons/ECM/ECM.gd")
 
 func _exit_tree():
 	# Clean-up of the plugin goes here
 	remove_custom_type("Entity")
 	remove_custom_type("Component")
 	remove_custom_type("Manager")
+	
+	remove_autoload_singleton("ECM")
 	
